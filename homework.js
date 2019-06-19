f75566d814971a5026ebd3710cc1559b68162528
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-06-19 17:00:06 +0300

    Update homework.js

--- homework.js
+++ homework.js
@@ -15,3 +15,27 @@ function requestApi(url) {
       const request = new XMLHttpRequest();
       request.open('GET', url);
       request.send();
+
+      request.addEventListener('readystatechange', () => {
+            if (request.readyState !== 4) return;
+
+            if (request.status !== 200) {
+                  console.log('error: ' + request.status);
+                  return;
+            }
+            const output = JSON.parse(request.responseText);
+
+            let inner = '';
+
+            output.results.forEach(function (item){
+                  let nameItem = item.name || item.title;
+                  let dateItem = item.first_air_date || item.release_date;
+                  /* console.log(nameItem); */
+                  inner += '<div class="col-2">' + nameItem + '</div>' 
+                  inner += '<div class="col-2">' + dateItem + '</div>';
+            });
+
+
+            movie.innerHTML = inner;
+});
+}
