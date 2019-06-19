60fee48f86e166b2db5b5d7a8ef97fd7a6073544
Evgen Tikhonov <tihonovaelena1503@gmail.com>
2019-06-19 16:59:12 +0300

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,2 +1,9 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
+
+function apiSearch(event) {
+      event.preventDefault();
+      const searchText = document.querySelector('.form-control').value;
+      const server = 'https://api.themoviedb.org/3/search/multi?api_key=ead41c3eaac089640f31601bd088ab4e&language=ru&query=' + searchText;
+      requestApi(server);
+}
